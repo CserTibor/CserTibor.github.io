96ff6272365ec04d1a7bc1b33a51e18e6466bed1
T1bor <35879795+Tibi47504@users.noreply.github.com>
2018-03-25 23:38:36 +0200

    Portfolio v1

--- script.js
+++ script.js
@@ -1,66 +1,22 @@
-var a = "Rock";
-var b = "Paper";
-var c = "Scissors";
 
-function rockButtonClicked() {
-  var player = a;
-  var computer = Math.floor(Math.random() * 3 + 1);
-  if (computer == 1) {
-    eredmeny = a;
-  } else if (computer == 2) {
-    eredmeny = b;
-  } else {
-    eredmeny = c;
-  }
-  document.getElementById("control").innerHTML =
-    "Player: " + player + " | " + "Computer: " + eredmeny;
-  if (player == eredmeny) {
-    document.getElementById("inner").innerHTML = "Draw!";
-  } else if (eredmeny == b) {
-    document.getElementById("inner").innerHTML = "Computer won!";
-  } else {
-    document.getElementById("inner").innerHTML = "You won!";
-  }
+function open(val){
+location.href = val;
 }
 
-function paperButtonClicked() {
-  var player = b;
-  var computer = Math.floor(Math.random() * 3 + 1);
-  if (computer == 1) {
-    eredmeny = a;
-  } else if (computer == 2) {
-    eredmeny = b;
-  } else {
-    eredmeny = c;
-  }
-  document.getElementById("control").innerHTML =
-    "Player: " + player + " | " + "Computer: " + eredmeny;
-  if (player == eredmeny) {
-    document.getElementById("inner").innerHTML = "Draw!";
-  } else if (eredmeny == c) {
-    document.getElementById("inner").innerHTML = "Computer won!";
-  } else {
-    document.getElementById("inner").innerHTML = "You won!";
-  }
-}
+function mybutton(x) {
+	x.classList.toggle("change");
+}var coll = document.getElementsByClassName("collapsible");
+var i;
 
-function scissorsButtonClicked() {
-  var player = c;
-  var computer = Math.floor(Math.random() * 3 + 1);
-  if (computer == 1) {
-    eredmeny = a;
-  } else if (computer == 2) {
-    eredmeny = b;
-  } else {
-    eredmeny = c;
-  }
-   document.getElementById("control").innerHTML =
-    "Player: " + player + " | " + "Computer: " + eredmeny;
-  if (player == eredmeny) {
-    document.getElementById("inner").innerHTML = "Draw!";
-  } else if (eredmeny == a) {
-    document.getElementById("inner").innerHTML = "Computer won!";
-  } else {
-    document.getElementById("inner").innerHTML = "You won!";
-  }
+for (i = 0; i < coll.length; i++) {
+	coll[i].addEventListener("click", function() {
+	this.classList.toggle("active");
+	var content = this.nextElementSibling;
+		if (content.style.maxHeight){
+		content.style.maxHeight = null;
+		}
+	else {
+		content.style.maxHeight = content.scrollHeight + "px";
+		} 
+	});
 }
